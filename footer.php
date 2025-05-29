27d023d3c6438c121241bbeca9d95d7c199765b3
sujeet301 <sujeetwebdev@gmail.com>
2025-05-29 20:56:57 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+
+<footer class="custom-footer">
+  <div class="footer-content">
+    <div class="footer-section brand">
+      <h2>CalcuZone.com</h2>
+    </div>
+    <div class="footer-section contact">
+      <h3>Contact</h3>
+      <input type="email" placeholder="Insert email here" />
+      //  <p>Address line one<br />Address line two</p> 
+    </div>
+    <div class="footer-section links">
+      <a href="#">ABOUT US</a>
+      <a href="#">REPORT A BUG</a>
+      <a href="#">FEEDBACK</a>
+      <a href="#">CONTACT US</a>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <p>Copyright © Footer 2024. All rights reserved.</p>
+  </div>
+</footer>
+
+</body>
+</html>
+
+
+?>
